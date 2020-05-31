9298e3a2bab686d2d0c1c09af7312ac765e2de1c
annushka34 <annsamoluk82@gmail.com>
2020-05-31 09:40:04 +0200

    перевантаження Friend

--- Perevantazennja/Perevantazennja/ПеревантаженняFriend.cpp
+++ Perevantazennja/Perevantazennja/ПеревантаженняFriend.cpp
@@ -1,20 +1,66 @@
 #include"Point.h"
 
+class MyClass
+{
+private:
+	int x;
+public:
+	MyClass(int x):x(x)
+	{
+			
+	}
+	void Show()
+	{
+		cout << x << endl;
+	}
+	void SetX(int x)
+	{
+		this->x = x;
+	}
+	friend void ShowMyClass(MyClass& obj);
+	friend MyClass operator+ (MyClass& obj1, MyClass& obj2);
+	friend int operator+ (int a, MyClass& obj2);
+	friend MyClass operator+ (MyClass& obj1, int a);
+};
+
+void ShowMyClass(MyClass& obj)
+{
+	cout << "x = " << obj.x << endl;
+}
+
+MyClass operator+ (MyClass& obj1, MyClass& obj2)
+{
+	MyClass obj(0);
+	obj.x = obj1.x + obj2.x;
+	return obj;
+}
+int operator+ (int a, MyClass& obj)
+{
+	return a + obj.x;
+}
+MyClass operator+ (MyClass& obj, int a)
+{
+	MyClass obj1(0);
+	obj1.x = obj.x + a;
+	return obj1;
+}
+
+
+
 void main()
 {
 	srand(time(0));
 	SetConsoleOutputCP(1251);
 	
-	Point p1(10, 30);
-	Point p2(3, 0);
-	p1.Show();
-	p2.Show();
-	Point p3 = p1 - p2;
-	p3.Show();
+	MyClass obj1(10);
+	MyClass obj2(20);
+	MyClass obj3 = obj1 + obj2;
+	ShowMyClass(obj3);
 
-	Point p4;
-	p4 = p3;
-	p4.Show();
+	int y = 5 + obj3;
+	cout << y << endl;
 
+	MyClass obj4 = obj3 + 5;
+	ShowMyClass(obj4);
 	//* / + - (*) () [] -> > < == <= >= ! != << >> new delete 
 }
