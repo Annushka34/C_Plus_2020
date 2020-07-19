89326ef38b148f4789316a6cbe6223f5dce47e0c
annushka34 <annsamoluk82@gmail.com>
2020-07-19 12:04:53 +0200

    STL

--- 9_12_STL_2/STL1/Array.cpp
+++ 9_12_STL_2/STL1/Array.cpp
@@ -1,20 +1,30 @@
 //#include <string>
 //#include <iterator>
 //#include <iostream>
-//#include <algorithm>
+////#include <algorithm>
 //#include <array>
 //using namespace std;
 //
 //int main()
 //{
-//	array<int,3> myArray1{ {2,4,5} };
-//	array<int, 3> myArray2 = { 3,5,6 };
+//	////// конструктор использует агрегатный инициализатор
+//	//array<int, 3> a1{ { 1,2,3 } };  // требуются двойные фигурные скобки,
+//	//array<int, 3> a2 = { 1, 2, 3 }; // за исключением операций присваивания
+//	//array<string, 2> a3 = { { "a", "b"} };
+//	//int a{ 5 };
+//	array<int,5> myArray1{ {2,4,5} };
+//	array<int, 5> myArray2 = { 3, 18, 7, 9, 13 };
 //	array<string, 13> myArrayStr = { "hello","bye" };
-//	sort(myArray1.begin(), myArray1.end());
-//	cout<<myArrayStr.max_size()<<endl;
-//	cout<< myArrayStr.size()<<endl;
+//	//sort(myArray1.begin(), myArray1.end());
+//	cout<< myArray2.max_size()<<endl;
+//	cout<< myArray2.size()<<endl;
+//
+//	//// поддерживается ranged for цикл
+//	//for (auto& s : a3)
+//	//	std::cout << s << ' ';
+//
 //	cout << "\n-------------my arr string-----------------------\n";
-//	for (auto el : myArrayStr)
+//	for (string el : myArrayStr)
 //	{
 //		cout << el << " ";
 //	}
@@ -25,22 +35,59 @@
 //		cout << el << " ";
 //	}
 //	cout << "\n------------------------------------\n";
+//	//  data  - get pointer (iterator) on first element - myArray1.data()
+//	cout << *(myArray1.data()+1);//   value [1]
+//	cout << *myArray1.data();//   value [0]
+//	cout << *(myArray1.data()+2)<<endl;//   value [2]
 //
-//	//myArray1.assign(1);
-//	cout << *(myArray1.data()+1);
-//		cout << *myArray1.data();
-//	cout << *(myArray1.data()+2);
+//	auto iter = myArray1.data();
+//	cout << typeid(iter).name() << endl;
 //
-//	cout<<myArray1.back();
-//	myArray1.fill(5);
+//	cout<<myArray1.back();//--- get last element
+//	//myArray1.fill(5);
 //	myArray1.swap(myArray2);
-//
-//	reverse_copy(myArray2.begin(), myArray2.end(), ostream_iterator<int>(cout, " "));
-//
-//	ostream_iterator<string> it (cout, ",");
+//	cout << "\n---------------------------after swap----------------\n";
+//	for (auto el : myArray1)
+//	{
+//		cout << el << " ";
+//	}
 //	cout << endl;
-//	copy(myArrayStr.begin(), myArrayStr.end()-2, it);
 //
+//	array<int, 5>::iterator itB = myArray1.begin();
+//	cout << typeid(itB).name() << endl;
+//	auto itE = myArray1.end();
+//	//cout << myArray1[2];
+//	//cout<< myArray1.at(2);
+//
+//	cout << "\n-------------ITERATOR------------------\n\n";
+//	itB++;
+//	cout << (*itB) << endl;
+//	itB = myArray1.begin();
+//
+//	for (auto it = myArray1.begin(); it < myArray1.end(); it++)
+//	{
+//		cout << "el: " << (*it) << endl;
+//	}
+//	cout << "\n-------------REV ITERATOR------------------\n\n";
+//	array<int, 5>::const_reverse_iterator itRB = myArray1.rbegin();
+//	for (auto it = myArray1.rbegin(); it < myArray1.rend(); it++)
+//	{
+//		cout << "el: " << (*it) << endl;
+//	}
+//
+//	
+//	copy(myArray1.begin(), myArray1.end() - 2, myArray2.begin());
+//	cout << "\n-------------COPY------------------\n\n";
+//	/*for (auto it = myArray2.begin(); it < myArray2.end(); it++)
+//	{
+//		cout << "el: " << (*it) << endl;
+//	}*/
+//
+//	auto osIt = ostream_iterator<int>(cout, ",    ");
+//	//copy(myArray2.begin(), myArray2.end(), osIt);
+//	reverse_copy(myArray2.begin(), myArray2.end(), osIt);
+//
+//	
 //	array<string, 13>::iterator it1 = myArrayStr.begin();
 //	cout << "3___________________________________\n";
 //	for (it1; it1 < myArrayStr.cend(); it1++)
@@ -50,19 +97,14 @@
 //	}
 //	cout << endl;
 //	cout << "4___________________________________\n";
-//	ostream_iterator<string> it2(cout, " : ");
-//	copy(myArrayStr.begin(), myArrayStr.end(), it2);
-//	//// конструктор использует агрегатный инициализатор
-//	array<int, 3> a1{ { 1,2,3 } };  // требуются двойные фигурные скобки,
-//	array<int, 3> a2 = { 1, 2, 3 }; // за исключением операций присваивания
-//	array<string, 2> a3 = { { "a", "b"} };
+//	//   stream iterator make output in console
+//	copy(myArrayStr.begin(), myArrayStr.end(), ostream_iterator<string>(cout, " : "));
+//	
+//	cout << endl;
+//	//// поддерживаются обобщённые алгоритмы
+//	sort(myArray1.begin(), myArray1.end());
+//	reverse_copy(myArray1.begin(), myArray1.end(), ostream_iterator<int>(cout, " "));
 //
-//	// поддерживаются обобщённые алгоритмы
-//	sort(a1.begin(), a1.end());
-//	reverse_copy(a2.begin(), a2.end(), ostream_iterator<int>(cout, " "));
-//
-//	// поддерживается ranged for цикл
-//	for (auto& s : a3)
-//		std::cout << s << ' ';
+//	
 //	system("pause");
 //}
--- 9_12_STL_2/STL1/stringIter.cpp
+++ 9_12_STL_2/STL1/stringIter.cpp
@@ -37,53 +37,64 @@
 //	cout << "str size:" << str.size() << endl;
 //	cout << "str capacity:" << str.capacity() << endl;
 //
-//	//cout << str<<endl;
+//	////cout << str<<endl;
 //
+//	cout << "\n---INDEX 3--\n";
+//	cout << str[3] << endl;
 //	cout<<str.at(3)<<endl;
 //
-//	//str.erase();
-//
+//	cout << "\n\n---ERAZE--\n";
+//	str.erase();
 //	cout<<" size "<<str.length()<<endl;
 //	cout<<str.size()<<endl;
-//	cout<<str.capacity()<<endl;
-//	cout<<str[3]<<endl;
+//	cout<<"capacity: "<<str.capacity()<<endl;
+//	str.shrink_to_fit();
+//	cout << "capacity after shrink to fit: " << str.capacity() << endl;
 //
+//	/// <summary>
+//	/// convert to char * arr
+//	/// </summary>
 //	const char*strCH = str.c_str();
 //
 //	char*arr = "kdhfaakhf";
 //	str = arr;
-//	//str1 = str;
+//	str1 = str;
+//	cout << &str << endl;
+//	cout << &str1 << endl;
 //
 //	cout << "\n compare: " << str.compare(str1) << endl;
-//	cout << "\n compare: " << (str1==str) << endl;
+//	cout << "\n compare: " << (str1 == str) << endl << endl;
 //
+//	cout << str << endl;
 //	str.erase(1, 3);
 //	cout << str << endl;
-//	//str = "aa";
+//	////str = "aa";
 //	cout << "find " << str.find("aa")<<endl;
-//	str.replace(str.find("aa"),2, "bb");
+//	str.replace(str.find("aa"),4, "bb");
 //	cout << str << endl;
 //	cout<<"max size "<<str.max_size()<<endl;
-//	cout<<"find "<<str.find_first_of('a') << endl;
-//	cout << "find last " << str.find_last_of('a') << endl;
-//	cout << "find last not" << str.find_first_not_of("adr") << endl;
+//
+//	cout<<"find "<<str.find_first_of('b') << endl;
+//	cout << "find last " << str.find_last_of('b') << endl;
+//	cout << "find last not" << str.find_first_not_of("bb") << endl;
+//	
 //	cout << "insert" << str.insert(2,"00000")<<endl;
 //	cout << str << endl;
-//	cout << "" << str.replace(3, 1, "11")<<endl;
-//	cout << str<<endl;	
-//	str = "hello world";
+//	//cout << "" << str.replace(3, 1, "11")<<endl;
+//	//cout << str<<endl;	
+//	//str = "hello world";
 //
-//	//string::iterator itBegin;
-//	//string::iterator itEnd;
+//	string::iterator itBegin;
+//	string::iterator itEnd;
 //
-//	//itBegin = str.begin();
-//	//itEnd = str.end();
+//	itBegin = str.begin();
+//	itEnd = str.end();
 //
-//	//while (itBegin != itEnd)
-//	//{
-//	//	cout << *itBegin << ' ';
-//	//	itBegin++;
-//	//}
+//	while (itBegin != itEnd)
+//	{
+//		cout << *itBegin << ' ';
+//		itBegin++;
+//	}
 //	//cout << endl;
 //	//for (auto it = str.begin(); it < str.end(); it++)
 //	//{
@@ -107,7 +118,7 @@
 //
 //	ostream_iterator<char> ostr(cout, ", ");
 //
-//	copy(str.begin(), str.end(), ostr);
+//	copy(str.begin()+2, str.end()-2, ostr);
 //
 //	//cout << str << endl;
 //	system("pause");
