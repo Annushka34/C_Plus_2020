e97a3d2f7e729a06287a8247650623c3e7bb1ab6
annushka34 <annsamoluk82@gmail.com>
2020-07-26 09:48:56 +0200

    list

--- 9_12_STL_2/STL1/list.cpp
+++ 9_12_STL_2/STL1/list.cpp
@@ -1,117 +1,197 @@
-//#include<iostream>
-//#include<list>
-//#include<algorithm>
-//#include<iterator>
-//
-//#include<string>
-//
-//using namespace std;
-//
-//void main()
-//{
-//	list<int> l(5,2);
-//	list<string>lS{ "aa","bb" };
-//	//---can't do this
-//	//cout<<l[2];
-//	//l.at
-//	l.insert(l.begin(), 3);
-//	cout << l.size()<<endl;
-//	ostream_iterator<int> it(cout, " ");
-//	copy(l.begin(), l.end(), it);
-//
-//	list<string>::iterator itS = lS.begin();
-//	while (itS != lS.end())
-//	{
-//		cout<<*itS << " ";
-//		itS++;
-//	}
-//
-//	lS.push_back("vv");
-//	cout<<lS.size() << endl;
-//	cout<<lS.front();
-//	lS.pop_front();
-//	cout << lS.size() << endl;
-//	lS.emplace(lS.begin(), "fff");
-//	ostream_iterator<string> itS1(cout, " ");
-//	copy(lS.begin(), lS.end(), itS1);
-//
-//	list<list<int>> two(2, list<int>{3,4,5});
-//	two.push_back(list<int>{8, 7, 16, 48});
-//	two.push_front(list<int> {5});
-//
-//	list<list<int>>::iterator itS2 = two.begin();
-//	typedef list<list<int>>::iterator myIt;
-//	myIt itS5 = two.begin();
-//	cout << "\n---------------------arr two-------\n";
-//	for (; itS5!=two.end(); itS5++)
-//	{
-//		for (list<int>::iterator it0 = (*itS5).begin(); it0!= (*itS5).end(); it0++)
-//		{
-//			cout << *it0 << " ";
-//		}
-//		cout << endl;
-//	}
-//
-//	cout << "\n---------------------arr two-------\n";
-//
-//	while (itS2 != two.end())
-//	{
-//		list<int>::iterator itS22 = (*itS2).begin();
-//		while (itS22 != (*itS2).end())
-//		{
-//			cout << *itS22 << " "; itS22++;
-//		}
-//		cout << "\n ";
-//		itS2++;
-//	}
-//
-//	for (auto el : two)
-//	{
-//		for (auto el1 : el)
-//		{
-//			cout << el1 << " ";
-//		}
-//		cout << endl;
-//	}
-//	list<int>l2;
-//
-//	l2 = l;
-//	cout << "adress l2: ";
-//	cout << &l2<<endl;
-//	cout << "adress l: ";
-//	cout << &l<<endl;
-//	l2 = { 2,3,5,8,11,6 };
-//	for (auto el : l2)
-//	{
-//		cout << el << " ";
-//	}
-//	//l2.sort();
-//	//l2.insert(l2.begin(), 8);
-//	list<int>::iterator it5 = l2.begin();
-//	list<int>::iterator it6 = l2.begin();
-//	it6++;
-//	
-//	advance(it6, 2);
-//	auto it7 = next(it6, 2);
-//	//can't do this!!!
-//	//cout << *(it + 2);
-//
-//	l2.erase(it5, it6);
-//	cout << endl;
-//	l2.insert(l2.begin(), 5,2);
-//	cout << "\n---------------------arr l2-------\n";
-//	for (auto el : l2)
-//	{
-//		cout << el << " ";
-//	}
-//	//---зсув масиву на 3 (it6 - begin)
-//	rotate(l2.begin(), it6, l2.end());
-//	cout << endl;
-//	cout << "\n--------------afte rotate-------arr l2-------\n";
-//
-//	for (auto el : l2)
-//	{
-//		cout << el << " ";
-//	}
-//	system("pause");
-//}
+#include<iostream>
+#include<list>
+#include<algorithm>
+#include<iterator>
+
+#include<string>
+
+using namespace std;
+
+class MyClass
+{
+public:
+	string name;
+	int age;
+	//int* arr;
+	//ctor - tab tab -  конструктор 
+	MyClass(string n, int a)
+	{
+		name = n;
+		age = a;
+	}
+	void Show()
+	{
+		cout << name << age;
+	}
+};
+
+ostream& operator << (ostream& out, MyClass& obj)
+{
+	out << obj.name << " - -" << obj.age << endl;
+	return out;
+}
+
+void main()
+{
+	list<int> l(5,2);
+	list<string>lS{ "aa","bb","aaaaa", "3", "tttt" };
+	//---can't do this
+	//cout<<l[2];
+	//l.at
+
+	/*l.insert(l.begin(), 3);
+	cout << l.size()<<endl;*/
+
+	/*ostream_iterator<int> it(cout, " ");
+	copy(l.begin(), l.end(), it);*/
+	list<string>::iterator itS = lS.begin();
+	while (itS != lS.end())
+	{
+		cout << *itS << " ";
+		itS++;
+	}
+	cout << "\nREVERSE ITERATOR: \n";
+	list<string>::reverse_iterator itSR = lS.rbegin();
+	while (itSR != lS.rend())
+	{
+		cout<<*itSR << " ";
+		itSR++;
+	}
+
+	cout << endl;
+	lS.push_back("vv");
+	cout<<"SIZE= "<<lS.size() << endl;
+	cout<<"front: "<<lS.front()<<endl;
+	lS.pop_front();
+	cout << "SIZE= " << lS.size() << endl;
+
+	lS.emplace(lS.begin(), "fff");//cant insert in free positiion
+	ostream_iterator<string> itS1(cout, " ");
+	copy(lS.begin(), lS.end(), itS1);
+
+
+	list<MyClass> listMyClasses{ MyClass("Olex", 20), MyClass("Irina", 22) };
+	for (auto el: listMyClasses)
+	{
+		//el.Show();
+		cout << el << endl;
+	}
+	list<MyClass>::iterator itM = listMyClasses.begin();
+	while (itM != listMyClasses.end())
+	{
+		//(*itM).Show();
+		cout << (*itM) << " ";
+		itM++;
+	}
+
+	list<list<int>> two(2, list<int>{3,4,5});
+	/*list<list<int>> two2
+	{
+		list<int>{1,7,8},
+		list<int>{3,2,8,6,12}
+	};*/
+	two.push_back(list<int>{8, 7, 16, 48});
+	two.push_front(list<int> {5});
+	//5
+	//3 4 5
+	//3 4 5
+	//8 7 16 48
+
+	for (list<int> list : two)
+	{
+		for (int el: list)
+		{
+			cout << el << " ";
+		}
+		cout << endl;
+	}
+
+	list<list<int>>::iterator itS2 = two.begin();
+	typedef list<list<int>>::iterator myIt;
+	
+	
+	cout << "\n---------------------arr two-------\n";
+	for (myIt itS5 = two.begin(); itS5!=two.end(); itS5++)
+	{
+		//two.push_back(list<int>{1, 2, 3, 45}); - неможна змніювати коллекцію коли вже визначено ітератор
+		list<int> line = (*itS5);//  розіменований ітератор це і-й рядок
+		line.push_front(1);
+		list<int>::iterator itLine = line.begin();//   ітератор на початок лінії
+		while(itLine != line.end())
+		{
+			cout << (*itLine) << " ";
+			itLine++;
+		}
+		cout << endl;
+	}
+
+	//cout << "\n---------------------arr two-------\n";
+
+	//while (itS2 != two.end())
+	//{
+	//	list<int>::iterator itS22 = (*itS2).begin();
+	//	while (itS22 != (*itS2).end())
+	//	{
+	//		cout << *itS22 << " "; itS22++;
+	//	}
+	//	cout << "\n ";
+	//	itS2++;
+	//}
+
+	MyClass m("aaa", 13);
+	MyClass m2("222", 18);
+	m = m2;//   operator =
+	MyClass m3 = m;//   constructor copy
+	MyClass* pm1 = new MyClass("fff", 15);
+	MyClass* pm2 = pm1;//   only one object!!!!!
+
+	list<int>l2;
+
+	l2 = l;
+	cout << "adress l2: ";
+	cout << &l2<<endl;
+	cout << "adress l: ";
+	cout << &l<<endl;
+	l2 = { 2,3,5,8,11,6 };
+	//for (auto el : l2)
+	//{
+	//	cout << el << " ";
+	//}
+	l2.sort();//  own method	
+	//sort(l2.begin(), l2.end());//  ALGORYPHM
+
+	l2.insert(l2.begin(), 8);
+	auto itL2 = l2.begin();
+	itL2++;
+	itL2++;
+	list<int>::iterator it5 = l2.begin();
+	list<int>::iterator it6 = l2.begin();
+	it6++;
+	//
+	advance(it6, 3);
+	auto it7 = next(it6, 2);
+	////can't do this!!!
+	////cout << *(it + 2);
+
+	l2.erase(it5, it6);
+	//cout << endl;
+	l2.insert(l2.begin(), 2);
+	l2.insert(l2.begin(), 77);
+	cout << "\n---------------------arr l2-------\n";
+	for (auto el : l2)
+	{
+		cout << el << " ";
+	}
+
+	cout << "it6=" << *it6 << endl;
+	////---зсув масиву на 3 (it6 - begin)
+	rotate(l2.begin(), it6, l2.end());
+	cout << endl;
+	cout << "\n--------------afte rotate-------arr l2-------\n";
+
+	for (auto el : l2)
+	{
+		cout << el << " ";
+	}
+	system("pause");
+}
--- 9_12_STL_2/STL1/vectorFile.cpp
+++ 9_12_STL_2/STL1/vectorFile.cpp
@@ -8,7 +8,7 @@
 //
 //	void WriteToFile(vector<int> v)
 //	{
-//		ofstream out_file("vector.bin", ios::binary | ios::out);
+//		ofstream out_file("vector.bin", ios::binary | ios::out);		
 //		out_file.write((const char*)&v.front(), v.size() * sizeof(int));
 //	}
 //
@@ -18,45 +18,29 @@
 //		//to_save.resize(100);
 //		ifstream in_file("vector.bin", ios::binary);
 //		in_file.read((char*)&v.front(), v.size() * sizeof(int));
-//
+//		//---вивід в консоль
 //		ostream_iterator<int>ositer(cout, ",");
 //		copy(v.begin(), v.end(), ositer);
 //		cout << endl;
 //	}
+//
 //	void WriteToFile1(vector<int> v)
 //	{
-//		ofstream outF("vec.dat");
+//		ofstream outF("vec.txt");
 //		ostream_iterator<int> ositer(outF, " ");
-//		copy(v.cbegin(), v.cend(), ositer);
+//		copy(v.cbegin(), v.cend(), ositer);//---(звідки(початок), звідки(кінець), куди)
 //	}
 //	void ReadFromFile1()
 //	{	
 //		vector <int> v(100);
-//		ifstream inFile("vec.dat");
+//		ifstream inFile("vec.txt");
 //		istream_iterator<int> fileIt(inFile);
 //		istream_iterator<int> endF;
-//		copy(fileIt, endF, back_inserter(v));
+//		copy(fileIt, endF, back_inserter(v));//front_inserter
+//		//copy(fileIt, endF, v.begin());
 //
-//
+//		//---вивід в консоль
 //		ostream_iterator<int>ositer(cout, ",");
-//		copy(v.begin(), v.end(), ositer);
-//		cout << endl;
-//	}
-//	void WriteToFile2(vector<char> v)
-//	{
-//		ofstream outF("vecStr.dat", ios::trunc);
-//		ostream_iterator<char> ositer(outF, " ");
-//		copy(v.cbegin(), v.cend(), ositer);
-//	}
-//	void ReadFromFile2()
-//	{
-//		vector <char> v(10);
-//		ifstream inFile("vecStr.dat");
-//		istream_iterator<char> fileIt(inFile);
-//		istream_iterator<char> endF;
-//		copy(fileIt, endF, back_inserter(v));
-//
-//		ostream_iterator<char>ositer(cout, ", ");
 //		copy(v.begin(), v.end(), ositer);
 //		cout << endl;
 //	}
@@ -64,125 +48,131 @@
 //
 //void main()
 //{
-//	vector<int> v(5, 2);
-//	vector<int> v1{ 2,3,5,7,8 };
-//	vector<int> v2(v1);
-//	vector<int> v3 = v2;
-//	//---size= 10 elements 0
-//	vector<int> test(10);
-//	cout << test.size();
-//	cout << "\n" << test[2] << endl<<"----------------------------\n";
+//	vector<int> v{ 5, 2, 7, 10, 13 };
 //
-//	for (int el : v1)
-//	{
-//		cout << el << " ";
-//	}
+//	WriteToFile(v);
+//	WriteToFile1(v);
 //
-//	cout <<"size "<< v1.size() << endl;
-//	cout << "capacity " << v1.capacity() << endl;
-//	v1.clear();
-//	cout << "size " << v1.size() << endl;
-//	cout << "capacity " << v1.capacity() << endl;
-//	v1.reserve(100);
-//	cout << "size " << v1.size() << endl;
-//	cout << "capacity " << v1.capacity() << endl;
-//	v1.push_back(7);
-//	v1.shrink_to_fit();
-//	cout << "size " << v1.size() << endl;
-//	cout << "capacity " << v1.capacity() << endl;
-//
-//	v1 = { 2,3,4,5,6,7 };
-//	v1[3] = 5;
-//	v1.at(2) = 0;
-//	for (int el : v1)
-//	{
-//		cout << el << " ";
-//	}
-//
-//	typedef vector<int>::reverse_iterator myIter;
-//	myIter it = v1.rbegin();
-//	auto it1 = v1.rend();
-//
-//	while (it!=it1)
-//	{
-//		cout << *it;
-//		it++;
-//	}
-//
-//	v1.emplace(v1.begin()+2, 200);
-//	cout << endl;
-//	cout << "-------------------------------\n";
-//	ostream_iterator<int> itStream(cout, " : ");
-//	copy(v1.begin(), v1.end(), itStream);
-//
-//	cout << endl;
-//	cout << "-------------------------------\n";
-//	v1.erase(v1.begin() + 3, v1.end() - 2);
-//	copy(v1.begin(), v1.end(), itStream);
-//	cout << "-------------------------------\n";
-//	cout << "-------------------------------\n";
-//
-//	cout << "insert 200 at 3 pos\n";
-//	v1.insert(v1.begin()+3, 200);
-//	cout << "size:" << v1.size() << " capacity:" << v1.capacity() << endl;
-//	cout << "insert 200 at 3 pos 5\n";
-//	v1.insert(v1.begin() + 3, 5,200);
-//	cout << "size:" << v1.size() << " capacity:" << v1.capacity() << endl;
-//	cout << endl;
-//	cout << "-------------------------------\n";
-//	copy(v1.begin(), v1.end(), itStream);
-//
-//	cout << endl;
-//
-//	vector<vector<int>> v4{ vector<int>(2, 3), vector<int>(3,6) };
-//	vector <vector<string>> v5(4, vector<string>{"hello", "bye"});
-//
-//	//auto it = v4.begin();
-//	vector<vector<int>>::iterator it4 = v4.begin();
-//	cout << "vector<vector>\n";
-//	for (; it4<v4.end(); it4++)
-//	{
-//		for (auto itJ=(*it4).begin();itJ<(*it4).end();itJ++)
-//		{
-//			cout << (*itJ) << " ";
-//		}
-//		cout << endl;
-//	}
-//	cout << "vector<vector> string \n";
-//	for (auto el : v5)
-//	{
-//		for (auto inEl : el)
-//		{
-//			cout <<inEl << " . ";
-//		}
-//		cout << endl;
-//	}
-//
-//	vector<string> v6{ "hi","go" };
-//	v6.push_back("star");
-//
-//	v5.push_back(v6);
-//
-//	cout << "vector<vector> string \n";
-//	for (auto el : v5)
-//	{
-//		for (auto inEl : el)
-//		{
-//			cout << inEl << " . ";
-//		}
-//		cout << endl;
-//	}
-//
-//
-//	vector<int> v7(100, 3);
-//	cout << "----------file with cast---------\n";
-//	WriteToFile1(v7);
+//	ReadFromFile();
+//	cout << "\nFILE 2\n";
 //	ReadFromFile1();
 //
-//	vector<char> v8(100, 'a');
-//	cout << "----------file with cast---------\n";
-//	WriteToFile2(v8);
-//	ReadFromFile2();
+//	////copy(v2.begin(), v2.end(), v1.begin());
+//	////---size= 10 elements 0
+//	//vector<int> test(10);
+//	//cout << test.size();
+//	//cout << "\n" << test[2] << endl<<"----------------------------\n";
+//
+//	//for (int el : v1)
+//	//{
+//	//	cout << el << " ";
+//	//}
+//
+//	//cout <<"size "<< v1.size() << endl;
+//	//cout << "capacity " << v1.capacity() << endl;
+//	//v1.clear();
+//	//cout << "size " << v1.size() << endl;
+//	//cout << "capacity " << v1.capacity() << endl;
+//	//v1.reserve(100);
+//	//cout << "size " << v1.size() << endl;
+//	//cout << "capacity " << v1.capacity() << endl;
+//	//v1.push_back(7);
+//	//v1.shrink_to_fit();
+//	//cout << "size " << v1.size() << endl;
+//	//cout << "capacity " << v1.capacity() << endl;
+//
+//	//v1 = { 2,3,4,5,6,7 };
+//	//v1[3] = 5;
+//	//v1.at(2) = 0;
+//	//for (int el : v1)
+//	//{
+//	//	cout << el << " ";
+//	//}
+//
+//	//typedef vector<int>::reverse_iterator myIter;
+//	//myIter it = v1.rbegin();
+//	//auto it1 = v1.rend();
+//
+//	//while (it!=it1)
+//	//{
+//	//	cout << *it;
+//	//	it++;
+//	//}
+//
+//	//v1.emplace(v1.begin()+2, 200);
+//	//cout << endl;
+//	//cout << "-------------------------------\n";
+//	//ostream_iterator<int> itStream(cout, " : ");
+//	//copy(v1.begin(), v1.end(), itStream);
+//
+//	//cout << endl;
+//	//cout << "-------------------------------\n";
+//	//v1.erase(v1.begin() + 3, v1.end() - 2);
+//	//copy(v1.begin(), v1.end(), itStream);
+//	//cout << "-------------------------------\n";
+//	//cout << "-------------------------------\n";
+//
+//	//cout << "insert 200 at 3 pos\n";
+//	//v1.insert(v1.begin()+3, 200);
+//	//cout << "size:" << v1.size() << " capacity:" << v1.capacity() << endl;
+//	//cout << "insert 200 at 3 pos 5\n";
+//	//v1.insert(v1.begin() + 3, 5,200);
+//	//cout << "size:" << v1.size() << " capacity:" << v1.capacity() << endl;
+//	//cout << endl;
+//	//cout << "-------------------------------\n";
+//	//copy(v1.begin(), v1.end(), itStream);
+//
+//	//cout << endl;
+//
+//	//vector<vector<int>> v4{ vector<int>(2, 3), vector<int>(3,6) };
+//	//vector <vector<string>> v5(4, vector<string>{"hello", "bye"});
+//
+//	////auto it = v4.begin();
+//	//vector<vector<int>>::iterator it4 = v4.begin();
+//	//cout << "vector<vector>\n";
+//	//for (; it4<v4.end(); it4++)
+//	//{
+//	//	for (auto itJ=(*it4).begin();itJ<(*it4).end();itJ++)
+//	//	{
+//	//		cout << (*itJ) << " ";
+//	//	}
+//	//	cout << endl;
+//	//}
+//	//cout << "vector<vector> string \n";
+//	//for (auto el : v5)
+//	//{
+//	//	for (auto inEl : el)
+//	//	{
+//	//		cout <<inEl << " . ";
+//	//	}
+//	//	cout << endl;
+//	//}
+//
+//	//vector<string> v6{ "hi","go" };
+//	//v6.push_back("star");
+//
+//	//v5.push_back(v6);
+//
+//	//cout << "vector<vector> string \n";
+//	//for (auto el : v5)
+//	//{
+//	//	for (auto inEl : el)
+//	//	{
+//	//		cout << inEl << " . ";
+//	//	}
+//	//	cout << endl;
+//	//}
+//
+//
+//	//vector<int> v7(100, 3);
+//	//cout << "----------file with cast---------\n";
+//	//WriteToFile1(v7);
+//	//ReadFromFile1();
+//
+//	//vector<char> v8(100, 'a');
+//	//cout << "----------file with cast---------\n";
+//	//WriteToFile2(v8);
+//	//ReadFromFile2();
 //
 //	system("pause");
 //}
